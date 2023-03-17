948be9260d6f779640360c97dfdde3d9b450e2ae
Lalit-7881 <lalitsonawane391@gmail.com>
2023-03-17 19:49:39 +0530

    REACT.JS

--- src/App.js
+++ src/App.js
@@ -2,28 +2,28 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Customers from './Customers'
-import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 
 
 class App extends Component {
   render() {
-    console.log("Host URL"+process.env.PUBLIC_URL);
+    console.log("Host URL" + process.env.PUBLIC_URL);
     return (
 
       <Router basename={process.env.PUBLIC_URL}>
         <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Simple React App</h1>
-        </header>
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Simple React App</h1>
+          </header>
           <Switch>
-                <Route exact path= "/" render={() => (
-                  <Redirect to="/customerlist"/>
-                )}/>
-                 <Route exact path='/customerlist' component={Customers} />
+            <Route exact path="/" render={() => (
+              <Redirect to="/customerlist" />
+            )} />
+            <Route exact path='/customerlist' component={Customers} />
           </Switch>
-      </div>
-    </Router>
+        </div>
+      </Router>
     );
   }
 }
